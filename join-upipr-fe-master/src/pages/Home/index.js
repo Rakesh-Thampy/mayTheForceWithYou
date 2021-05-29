e4fb86c578e4ee862af97ecff2febe4717b92bc2
Rakesh <rakesh17051995@gmail.com>
2021-05-29 21:22:00 +0530

    day 2 added redirect and loading func

--- join-upipr-fe-master/src/pages/Home/index.js
+++ join-upipr-fe-master/src/pages/Home/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import logo from './star-wars-logo.png';
 import './index.css';
 import axios from 'axios'
+import { useHistory } from 'react-router-dom'; 
+
 
 function HomePage() {
   // maintain all the states
+  const [query, setQuery] = React.useState("")
   const [data, setData] = React.useState([])
   const [isLoading, setLoading] = React.useState(false)
+  const [active, setActive] = React.useState(-1)
+
+  // calling history
+  const history = useHistory()
 
   // function to fetch data
   const fetchData = (val)=>{ 
@@ -21,22 +28,62 @@ function HomePage() {
   let timer
   function debounce(e){
     let {value} = e.target
-    timer && clearTimeout(timer)
-    timer = setTimeout(() => {
-      fetchData(value)
-    }, 500);
+    let code = e.keyCode
+    if(code !== 40 && code !== 13 && code !== 38){
+      timer && clearTimeout(timer)
+      timer = setTimeout(() => {
+        fetchData(value)
+      }, 500);
+    }
+    
   }
+  // handling the search query 
+  const handleChange = (e)=>{
+    setQuery(e.target.value)
+  }
+
+
+  // handling the cross button in search bar
+  const handleCross = ()=>{
+    setData([])
+    setQuery("")
+
+  }
+
+  // handling arrows to scroll through result
+  const handleArrows = (e)=>{
+    switch (e.keyCode) {
+      case 40: setActive( prev => prev +1)
+                break;
+      case 38: setActive( prev => prev -1)
+                break;
+      case 13: let info = data[active].name
+                history.push(`/person/${info}`)
+                break;
+      default:
+        break;
+    }
+  }
+
 
   return (
     <div>
       <div className="logo">
         <img src={logo} alt="Star Wars Logo" />
       </div>
-      <input className="search-input" placeholder="Search by name"onKeyUp = {(e)=> debounce(e)} />
-      <div className = "search-result">
-        {data?.map((item)=> (
-          <p>{item.name}</p>
-        ))}
+      {/*input box starts here  */}
+      <div onKeyUp = {(e)=> handleArrows(e)}>
+        <div className = 'search-input-container'>   
+          <input className="search-input" value = {query} placeholder="Search by name" onChange = {(e)=> handleChange(e)} onKeyUp = {(e)=>  debounce(e)} />
+          {data.length>0 && <div className = "search-input-cross" onClick = {handleCross}> X </div>}
+          {isLoading? <div className = "search-input-loader"></div> : <div className = "search-img-wrapper"> <img src="https://image.flaticon.com/icons/png/512/49/49116.png" alt="search" className = "search-img" /></div> }
+        </div>
+      {/*search result container starts here  */}
+        <div className = "search-result" style = {data.length > 0? {paddingTop: "20px"}: null}>
+          {data?.map((item, i)=> ( 
+            <div style = {active === i? {backgroundColor:"yellow", color:"#110B0B"}: null} onClick = {()=> history.push(`/person/${item.name}`)}>{item.name}</div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- join-upipr-fe-master/src/pages/Person/index.js
+++ join-upipr-fe-master/src/pages/Person/index.js
@@ -1,12 +1,62 @@
 import React from 'react';
 import './index.css';
+import StarwarsImage from '../../images/starWars_bg.png'
+import { useParams } from 'react-router';
+import axios from 'axios'
+import LoadingImg from '../../images/loader.gif'
+
 
 function Person() {
-  return (
-    <div className="person">
-      <h1>Luke Skywalker</h1>
-    </div>
-  );
+  // maintaing states
+  const {id} = useParams()
+  const [isLoading, setLoading] = React.useState(true)
+  const [data, setData] = React.useState({})
+  
+
+  React.useEffect(()=>{
+    fetchName(id)
+    setTimeout(() => {
+       setLoading(false)
+    }, 4000);
+  },[id])
+
+//  fetch data accoding to name in url
+  function fetchName(name){
+    axios.get(`https://swapi.dev/api/people/?search=${name}`)
+    .then(res=> setData(res.data.results[0]))
+    .catch((err)=> console.log(err))
+  }
+// starting loading page
+  if (isLoading) {
+    return(
+      <img src={LoadingImg} alt="loading" />
+    )
+  }else{
+    return (
+      <div className="person">
+        <div className = "person_card">
+          <h1> {data.name}</h1>
+          <div>
+            <h2> <span>Birth year:</span> {data.birth_year}</h2>
+            <h2> <span>Eye color:</span> {data.eye_color}</h2>
+          </div>
+          <div>
+            <h2> <span>Gender:</span> {data.gender}</h2>
+            <h2> <span>Height: </span> {data.height}</h2>
+          </div>
+          <div>
+            <h2> <span>Mass:</span> {data.mass}</h2>
+            <h2> <span>Skin color:</span> {data.skin_color}</h2>
+          </div>
+
+        </div>
+        <div className = "person-footer">
+          <img src={StarwarsImage} alt="img" />
+        </div>
+      </div>
+    )
+  }
 }
+
 
 export default Person;
